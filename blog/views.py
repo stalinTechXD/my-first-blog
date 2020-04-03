6476cf3b9c1aa1b3ed5cdd703de7c618118f7114
stali1234 <stalin.sangeethraj@gmail.com>
2020-04-03 16:04:30 -0400

    second commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
+def post_line(request):
+    posts = Post.objects.all()
+    return render(request,'blog/post_list.html',{'posts':posts})
